4eee841e9fe3d8bccc9131b11cba23bcc4f33241
yoninah2000 <s.yoninah@gmail.com>
2021-07-26 00:13:08 +0530

    final commit

--- main.js
+++ main.js
@@ -3,19 +3,24 @@ const tabContentItems = document.querySelectorAll('.tab-content-item');
 
 // Select tab content item
 function selectItem(e) {
-    // removeBorder();
+    // addBorder();
+    removeBorder();
     removeShow();
     // add border to current tab
-    // this.classList.add('tab-border');
+    this.classList.add('tab-border');
     // grab content item from the DOM
     const tabContentItem = document.querySelector(`#${this.id}-content`);
     // add show class
     tabContentItem.classList.add('show');
 }
 
-// function removeBorder() {
-//     tabItems.forEach(item => item.classList.remove('tab-border'));
+// function addBorder() {
+//     tabItems(item => item.classList.add('one'));
 // }
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
 
 function removeShow() {
     tabContentItems.forEach(item => item.classList.remove('show'));
